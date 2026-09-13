17aa39c7238c60c7114b9d64d5b5d4777434a210
Uchqun <83647411+UchqunTurdiyev@users.noreply.github.com>
2026-09-13 18:08:30 +0500

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "githup-ozi",
+  description: "Hello World - Next.js App Router",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
