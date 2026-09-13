edd52f09a871134f12c359c6ae3e6203e33e3c0e
Uchqun <83647411+UchqunTurdiyev@users.noreply.github.com>
2026-09-13 18:09:00 +0500

    Create Home component in page.js

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
